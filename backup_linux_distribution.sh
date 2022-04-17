a8235451a95e81c3bc099cf91caf687937106414
Andy Holst <andy.holst@protonmail.com>
2022-04-17 21:33:39 +0200

    Refined backup linux command

--- backup_linux_distribution.sh
+++ backup_linux_distribution.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env bash
 
+if [[ $# -eq 0 ]] ; then
+    echo 'Give at least a directory destination.'
+    exit 1
+fi
+
+dest="${1}"
+
 sudo rsync -aAX --delete / \
---exclude={"/dev/*","/proc/*","/sys/*","/tmp/*","/run/*","/mnt/*","/media/*","/swapfile","/lost+found","/home"} $(pwd)/manjaro
+--exclude={"/dev/*","/proc/*","/sys/*","/tmp/*","/run/*","/mnt/*","/media/*","/swapfile","/lost+found","/home"} "$(pwd)/${dest}"

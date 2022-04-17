2942e2596424605ba515081ea7062c4676de866b
Andy Holst <andy.holst85@gmail.com>
2022-04-17 20:00:05 +0200

    sync_git_repositories
    List only directories

--- sync_git_repositories.sh
+++ sync_git_repositories.sh
@@ -4,6 +4,6 @@
 # Sync git repositories from current directory
 ##############################################
 
-ls | xargs -P10 -I{} git -C {} fetch --all
-ls | xargs -P10 -I{} git -C {} checkout $(git remote set-head origin --auto >/dev/null 2>&1 && git rev-parse --abbrev-ref origin/HEAD | sed s:origin/::)
-ls | xargs -P10 -I{} git -C {} pull origin $(git branch --show-current)
+ls -d | xargs -P10 -I{} git -C {} fetch --all
+ls -d | xargs -P10 -I{} git -C {} checkout $(git remote set-head origin --auto >/dev/null 2>&1 && git rev-parse --abbrev-ref origin/HEAD | sed s:origin/::)
+ls -d | xargs -P10 -I{} git -C {} pull origin $(git branch --show-current)

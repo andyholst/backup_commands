f069733de1618d89c0601bee1ae3f6f3c1e7128b
Andy Holst <andy.holst85@gmail.com>
2022-04-17 20:04:19 +0200

    sync_git_repositories
    Execute sequentially.

--- sync_git_repositories.sh
+++ sync_git_repositories.sh
@@ -4,6 +4,6 @@
 # Sync git repositories from current directory
 ##############################################
 
-ls -d */ | xargs -P10 -I{} git -C {} fetch --all
-ls -d */ | xargs -P10 -I{} git -C {} checkout $(git remote set-head origin --auto >/dev/null 2>&1 && git rev-parse --abbrev-ref origin/HEAD | sed s:origin/::)
-ls -d */ | xargs -P10 -I{} git -C {} pull origin $(git branch --show-current)
+ls -d */ | xargs -I{} git -C {} fetch --all
+ls -d */ | xargs -I{} git -C {} checkout $(git remote set-head origin --auto >/dev/null 2>&1 && git rev-parse --abbrev-ref origin/HEAD | sed s:origin/::)
+ls -d */ | xargs -I{} git -C {} pull origin $(git branch --show-current)

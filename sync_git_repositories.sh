1d88a1c299ce24eb76124a06b5e4bddb8f594cd6
Andy Holst <andy.holst85@gmail.com>
2022-04-17 21:07:34 +0200

    sync_git_repositories
    Added git prune command to delete all obsolete remote branches.

--- sync_git_repositories.sh
+++ sync_git_repositories.sh
@@ -5,5 +5,6 @@
 ##############################################
 
 ls -d */ | xargs -I{} git -C {} fetch --all
+ls -d */ | xargs -I{} git -C {} fetch --prune
 ls -d */ | xargs -I{} git -C {} checkout $(git remote set-head origin --auto >/dev/null 2>&1 && git rev-parse --abbrev-ref origin/HEAD | sed s:origin/::)
 ls -d */ | xargs -I{} git -C {} pull origin $(git branch --show-current)

fe714e22eceba5a7b7c35a9dbfa37730b3cab5a7
Andy Holst <andy.holst@protonmail.com>
2022-04-23 17:28:35 +0200

    Refined git sync commands to be more verbose.

--- sync_git_repositories.sh
+++ sync_git_repositories.sh
@@ -4,7 +4,15 @@
 # Sync git repositories from current directory
 ##############################################
 
-ls -d */ | xargs -I{} git -C {} fetch --all
-ls -d */ | xargs -I{} git -C {} fetch --prune
-ls -d */ | xargs -I{} git -C {} checkout $(git remote set-head origin --auto >/dev/null 2>&1 && git rev-parse --abbrev-ref origin/HEAD | sed s:origin/::)
-ls -d */ | xargs -I{} git -C {} pull origin $(git branch --show-current)
+ls -d */ | xargs -I{} bash -c 'git -C {} rev-parse --show-toplevel && git -C {} rev-parse --abbrev-ref HEAD \
+	&& echo " Fetching. " && git -C {} fetch --all'
+ls -d */ | xargs -I{} bash -c 'git -C {} rev-parse --show-toplevel && git -C {} rev-parse --abbrev-ref HEAD \
+	&& echo " Setting origin main branch to $(git -C {} remote set-head origin --auto). " && git -C {} remote set-head origin --auto'
+ls -d */ | xargs -I{} bash -c 'git -C {} rev-parse --show-toplevel && git -C {} rev-parse --abbrev-ref HEAD \
+	&& echo " Checking out $(git -C {} rev-parse --abbrev-ref origin/HEAD). " \
+	&& git -C {} checkout $(git -C {} rev-parse --abbrev-ref origin/HEAD | sed s:origin/::)'
+ls -d */ | xargs -I{} bash -c 'git -C {} rev-parse --show-toplevel && git -C {} rev-parse --abbrev-ref HEAD \
+	&& echo " Pruning obsolete branches. " && git -C {} fetch --prune'
+ls -d */ | xargs -I{} bash -c 'git -C {} rev-parse --show-toplevel && git -C {} rev-parse --abbrev-ref HEAD \
+	&& echo " Pulling from origin $(git -C {} rev-parse --abbrev-ref origin/HEAD). " \
+	&& git -C {} pull origin $(git -C {} branch --show-current)'
